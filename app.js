3cb62a417373b7f9006e2233bc575e4dfce68c9e
Justin <schiller.justin@gmail.com>
2019-10-15 19:50:49 -0500

    Adding Sass file/folder structure. Beginning HTML markup of header section

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ app.get('/', function(req, res){
 });
 
 app.get('/campgrounds', function(req, res){
-   var campgrounds = [
-      {name: 'Salmon Creek', image: 'https://live.staticflickr.com/4567/37514240304_1a744f1fce_z.jpg'},
-      {name: 'Mountain View', image: 'https://www.yosemite.com/wp-content/uploads/2016/04/westlake-campground.png'},
-      {name: 'Granite Rock', image: 'https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_452,q_75,w_982/http://res.cloudinary.com/simpleview/image/upload/v1469218578/clients/lanecounty/constitution_grove_campground_by_natalie_inouye_417476ef-05c3-464d-99bd-032bb0ee0bd5.png'}
-   ]
-   
    res.render('campgrounds', {campgrounds: campgrounds});
 });
 
