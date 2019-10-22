fd38aa8050a8bb7b9f93861e32590a42ebb1a1c7
Justin <schiller.justin@gmail.com>
2019-10-22 12:07:25 -0500

    Developing script to transition opening and closing of main menu dropdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import { headerNoticeBtn } from './views/dom-loader';
-
-const hideHeaderNotice = () => {
-    document.querySelector('.header-notice').style.display = 'none';
-};
+import { elements } from './views/base';
+import * as homePage from './views/home-page';
 
 
 
-headerNoticeBtn.addEventListener('click', hideHeaderNotice);
+
+// Small 'X' icon in Header Notice section
+elements.headerNoticeBtn.addEventListener('click', function() {
+   homePage.hideElement(elements.headerNotice);
+});
+
+// Main menu button
+elements.mainMenuBtn.addEventListener('click', function() {
+   homePage.toggleDropdown(elements.mainMenuDropdown);
+});
+
+// Main menu item
+elements.mainMenuItem.addEventListener('mouseover', function(event) {
+   console.log(event.target);
+});
