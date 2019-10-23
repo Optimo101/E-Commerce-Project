0ae0f0bb2c2e9af8e1bee520098c64776fbda781
Justin <schiller.justin@gmail.com>
2019-10-23 07:48:27 -0500

    Completing function that allows user to close main menu when clicking anywhere outside of main menu

--- src/views/base.js
+++ src/views/base.js
@@ -2,8 +2,9 @@ export const elements = {
    headerNotice: document.querySelector('.header-notice'),
    headerNoticeBtn: document.querySelector('.header-notice__close-icon'),
    shadowOverlay: document.querySelector('.shadow-overlay'),
-   mainMenuBtn: document.querySelector('.main-menu__button'),
+   mainMenuBtn: document.querySelector('.main-menu__btn'),
    mainMenuDropdown: document.querySelector('.main-menu__dropdown'),
-   mainMenuItem: document.querySelector('.main-menu__item')
+   mainMenuItem: document.querySelector('.main-menu__item'),
+
 };
 
