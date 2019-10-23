d0e89880c2b584c5052cfbcd6df4c63b5dce0d75
Justin <schiller.justin@gmail.com>
2019-10-23 11:36:30 -0500

    Updated dropdown menu functions

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import * as homePage from './views/home-page';
 
 
 
-// Small 'X' icon in Header Notice section
+// Small 'X' button in Header Notice section
 elements.headerNoticeBtn.addEventListener('click', function() {
    homePage.hideElement(elements.headerNotice);
 });
 
-// Main menu button
+// Open/Close main menu
 elements.mainMenuBtn.addEventListener('click', function() {
    homePage.toggleDropdown(elements.mainMenuDropdown);
 });
@@ -19,7 +19,7 @@ document.addEventListener('click', function(event) {
    homePage.hideOnClickOutside(event, elements.mainMenuDropdown);
 });
 
-// Main menu item
-// elements.mainMenuItem.addEventListener('mouseover', function(event) {
-//    console.log(event.target);
-// });
+// Open/Close submenu
+elements.mainMenuItem.addEventListener('mouseover', function(event) {
+   console.log(event.target);
+});
