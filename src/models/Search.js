c1af04a747f0bf0e26020a77a1374150a7d460d5
Justin <schiller.justin@gmail.com>
2019-10-30 08:04:34 -0500

    Refined search model algorithm for better search results. Moved submenu funtions from index.js to submenuView.js

--- src/models/Search.js
+++ src/models/Search.js
@@ -5,20 +5,36 @@ export default class Search {
       this.query = query;
    }
 
+   formatQuery() {
+      const queryArray = this.query.trim().split(' ');
+      const addToString = "search=\'";
+
+      let newQueryArray = [];
+
+      queryArray.forEach(element => {
+         newQueryArray.push(addToString.concat(element) + "\'");
+      });
+
+      return newQueryArray.join('&');
+   };
+
    async getResults() {
       const baseURL = 'https://api.bestbuy.com/v1/';
       const apiType = 'products';
-      const catID = '&(categoryPath.id=abcat0700000)'
+      const keyWords = this.formatQuery();
+      console.log(keyWords);
+      const catID = '';
       const apiKey = 'MORTkmhIyQS3N3Pahuta4gSd';
       const sortOptions = '&sort=name.asc';
       const showOptions = '&show=sku,name,regularPrice';
       const pagination = `&pageSize=100`;
       const responseFormat = '&format=json';
 
-   
       try {
-         const response  = await axios.get(`${baseURL}${apiType}((search=${this.query})${catID})?apiKey=${apiKey}${sortOptions}${showOptions}${pagination}${responseFormat}`);
+         const response  = await axios.get(`${baseURL}${apiType}((${keyWords})${catID})?apiKey=${apiKey}${sortOptions}${showOptions}${pagination}${responseFormat}`);
+
          this.results = response.data.products;
+
       } catch (error) {
          console.error(error);
       }
