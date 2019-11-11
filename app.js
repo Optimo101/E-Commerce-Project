872452ccb137af05be48a27863180fb7b74bffca
Justin <schiller.justin@gmail.com>
2019-11-11 12:55:35 -0600

    Upating results route and moving API request to backend.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const    express     = require('express'),
+         app         = express(),
+         path        = require('path'),
+         newSearch   = require('./models/newSearch');
 
 // 
 // =================================================
@@ -12,16 +13,28 @@ app.set('view engine', 'ejs');
 
 // =================================================
 
-app.get('/', function(req, res){
+
+app.get('/', function(req, res) {
    res.render('home');
 });
 
-app.get('/results', function(req, res){
-   res.render('results/index');
+app.get('/results', async function(req, res) {
+
+   var search = new newSearch(req.query.search);
+
+   await search.getResults();
+   console.log(search.results);
+
+   res.render('index');
+
+});
+
+app.get('*', function(req, res) {
+   res.send('Sorry, page not found.')
 });
 
 
 
-app.listen(3000, 'localhost', function(){
+app.listen(3000, 'localhost', function() {
    console.log('Server has started on port 3000...');
 });
