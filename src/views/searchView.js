64ccdbcb8805a8ce316f8337c7b3a62e8d3d8bdc
Justin <schiller.justin@gmail.com>
2019-11-14 23:25:15 -0600

    Editing styling of main grid and product-thumb container so that images and layout are responsive

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -3,21 +3,21 @@ import { elements } from './base';
 const renderProductThumb = (product) => {
    const markup = 
    `<div class="product-thumb">
-      <a href="#" class="product-thumb__img-wrap">
+
+      <a class="product-thumb__top-content" href="#">
          <img src="${product.image}" alt="${product.name}" class="product-thumb__img">
       </a>
 
-      <div class="product-thumb__btn-wrap">
-         <button class="product-thumb__btn btn btn--small btn--black">
-            <i class="product-thumb__cart-icon fas fa-shopping-cart"></i>
-            Add to Cart
-         </button>
-         <button class="product-thumb__btn btn btn--small btn--black">
-            <i class="product-thumb__heart-icon far fa-heart"></i>
-         </button>
-      </div>
-      
-      <div class="product-thumb__desc-wrap">
+      <div class="product-thumb__middle-content">
+         <div class="product-thumb__btn-wrap">
+            <button class="product-thumb__btn btn btn--small btn--black">
+               <i class="product-thumb__cart-icon fas fa-shopping-cart"></i>
+               Add to Cart
+            </button>
+            <button class="product-thumb__btn btn btn--small btn--black">
+               <i class="product-thumb__heart-icon far fa-heart"></i>
+            </button>
+         </div>
          <div class="product-thumb__review-wrap">
             <i class="product-thumb__review-icon fas fa-star"></i>
             <i class="product-thumb__review-icon fas fa-star"></i>
@@ -25,9 +25,15 @@ const renderProductThumb = (product) => {
             <i class="product-thumb__review-icon fas fa-star"></i>
             <i class="product-thumb__review-icon product-thumb__review-icon--empty far fa-star"></i>
          </div>
-         <a href="#" class="product-thumb__title">${product.name}</a>
+         <div class="product-thumb__desc-wrap">
+            <a href="#" class="product-thumb__title">${product.name}</a>
+         </div>
+      </div>
+
+      <div class="product-thumb__bottom-content">
          <div class="product-thumb__price">$${product.regularPrice}</div>
-      </div> 
+      </div>
+
    </div>`;
 
    elements.resultsMainGrid.insertAdjacentHTML('beforeend', markup);
