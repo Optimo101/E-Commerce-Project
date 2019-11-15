ea07457235a09e06402b1b39dcc465fee1dedceb
Justin <schiller.justin@gmail.com>
2019-11-15 13:42:23 -0600

    Updated inner-layout of product-thumb from results page and added a number of reviews element

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -3,12 +3,31 @@ import { elements } from './base';
 const renderProductThumb = (product) => {
    const markup = 
    `<div class="product-thumb">
-
       <a class="product-thumb__top-content" href="#">
          <img src="${product.image}" alt="${product.name}" class="product-thumb__img">
       </a>
 
       <div class="product-thumb__middle-content">
+         <div class="product-thumb__desc-wrap">
+            <a href="#" class="product-thumb__title">${product.name}</a>
+         </div>
+
+         <div class="product-thumb__review-wrap">
+            <div class="product-thumb__review-stars">
+               <i class="product-thumb__review-icon fas fa-star"></i>
+               <i class="product-thumb__review-icon fas fa-star"></i>
+               <i class="product-thumb__review-icon fas fa-star"></i>
+               <i class="product-thumb__review-icon fas fa-star"></i>
+               <i class="product-thumb__review-icon product-thumb__review-icon--empty far fa-star"></i>
+            </div>
+            <div class="product-thumb__review-number">
+               (1,234)
+            </div>
+         </div>
+      </div>
+
+      <div class="product-thumb__bottom-content">
+         <div class="product-thumb__price">$${product.regularPrice}</div>
          <div class="product-thumb__btn-wrap">
             <button class="product-thumb__btn btn btn--small btn--black">
                <i class="product-thumb__cart-icon fas fa-shopping-cart"></i>
@@ -18,22 +37,7 @@ const renderProductThumb = (product) => {
                <i class="product-thumb__heart-icon far fa-heart"></i>
             </button>
          </div>
-         <div class="product-thumb__review-wrap">
-            <i class="product-thumb__review-icon fas fa-star"></i>
-            <i class="product-thumb__review-icon fas fa-star"></i>
-            <i class="product-thumb__review-icon fas fa-star"></i>
-            <i class="product-thumb__review-icon fas fa-star"></i>
-            <i class="product-thumb__review-icon product-thumb__review-icon--empty far fa-star"></i>
-         </div>
-         <div class="product-thumb__desc-wrap">
-            <a href="#" class="product-thumb__title">${product.name}</a>
-         </div>
       </div>
-
-      <div class="product-thumb__bottom-content">
-         <div class="product-thumb__price">$${product.regularPrice}</div>
-      </div>
-
    </div>`;
 
    elements.resultsMainGrid.insertAdjacentHTML('beforeend', markup);
