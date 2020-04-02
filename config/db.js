06c95d27234287b127fcfe638b0f1ad7ddb83dde
Justin <schiller.justin@gmail.com>
2020-04-01 23:06:31 -0500

    Updating database configuration info in db.js and .env files

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-   user: process.env.PGUSER,
-   host: process.env.PGHOST,
-   database: process.env.PGDATABASE,
-   password: process.env.PGPASS,
-   port: process.env.PGPORT
+   connectionString: process.env.DATABASE_URL,
+   ssl: true,
+   user: process.env.PGUSER, // PGUSER=eshopadmin
+   password: process.env.PGPASS, // PGPASS=admin
+   database: process.env.PGDATABASE // PGDATABASE=eshopdb
+
+   // user: process.env.PGUSER,
+   // host: process.env.PGHOST,
+   // database: process.env.PGDATABASE,
+   // password: process.env.PGPASS,
+   // port: process.env.PGPORT
 });
 
 module.exports = {
