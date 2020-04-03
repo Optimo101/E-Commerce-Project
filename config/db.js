8fe175bb1b59b714a778d0d5e935e91889a2fc04
Justin <schiller.justin@gmail.com>
2020-04-03 14:50:38 -0500

    Fixing issue with connect-pg-simple settings. Attempt to run settings locally was successful.

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
    connectionString: process.env.DATABASE_URL,
    ssl: true
-   // user: process.env.PGUSER,
-   // password: process.env.PGPASS,
-   // database: process.env.PGDATABASE
-
+   
    // user: process.env.PGUSER,
    // host: process.env.PGHOST,
    // database: process.env.PGDATABASE,
@@ -14,10 +11,11 @@ const pool = new Pool({
    // port: process.env.PGPORT
 });
 
-console.log('Connection String:', pool);
-
 module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
+   query: (text, params, callback) => {
+      return pool.query(text, params, callback)
+   },
+   getPool: () => {
+      return pool
+   }
 }
