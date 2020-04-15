9c5f5146acc1c12afdab83d4ab237b731c2c589d
Justin <schiller.justin@gmail.com>
2020-04-14 23:49:19 -0500

    Installing previous version of pg (version 7.17.1) to prevent deprecated warning on ssl configuration

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
    connectionString: process.env.DATABASE_URL,
-   ssl: {
-      rejectUnauthorized: true,
-   }
+   ssl: true
    
    // user: process.env.PGUSER,
    // host: process.env.PGHOST,
