7f2cc75f1c48459616f83126a77dcb077460c365
Justin <schiller.justin@gmail.com>
2020-04-18 15:30:24 -0500

    Troubleshooting Heroku deployment

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 // Parser for .env variables
 require('dotenv').config();
 
-console.log('process.env output:', process.env);
 console.log('process.env.PORT output:', process.env.PORT);
 console.log('process.env.NODE_ENV output:', process.env.NODE_ENV);
 
