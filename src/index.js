a0d35e22ea1d1abc577177592d1c8c476fcf0ab9
Justin <schiller.justin@gmail.com>
2020-04-20 17:41:17 -0500

    Fixing main menu so that it performs based purely on scree width less than 576px and not touchscreen recognition.

--- src/index.js
+++ src/index.js
@@ -505,7 +505,7 @@ const controlHeader = async () => {
       });
 
       // Open/close submenus
-      if (!state.isTouchScreen) {
+      if (!screen.width < 576) {
          submenuView.setUpSubmenuEvent('mouseover', submenuView.showSubMenu);
          submenuView.setUpSubmenuEvent('mouseleave', submenuView.hideSubMenu);
       } else {
